a279d196abe83b349341e6cae5d55f1c423ac7ee
cpage-pivotal <cpage@pivotal.io>
2021-11-07 15:34:25 -0600

    bugfixes

--- configure-ingress.sh
+++ configure-ingress.sh
@@ -2,7 +2,7 @@ set -x
 
 ytt -f ingress-resources.yaml -f values-ingress.yaml | kubectl apply -f-
 
-kubectl patch configmap config-domain- -n knative-serving -p "$(ytt -f patch-domain.yaml -f values-ingress.yaml | sed "s/key: //g" | sed "s/'//g")"
+kubectl patch configmap config-domain -n knative-serving -p "$(ytt -f patch-domain.yaml -f values-ingress.yaml | sed "s/key: //g" | sed "s/'//g")"
 
 kubectl patch configmap config-network -n knative-serving -p "$(ytt -f patch-network.yaml -f values-ingress.yaml)"
 
